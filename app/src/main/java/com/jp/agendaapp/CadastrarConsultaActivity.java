f516daaf767f562c4a9bc444f0a0132896d96569
Bruno Morais <brunomoraisti@gmail.com>
2018-04-28 17:35:36 -0300

    alterações

---
+++ app/src/main/java/com/jp/agendaapp/CadastrarConsultaActivity.java
@@ -0,0 +1,13 @@
+package com.jp.agendaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastrarConsultaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_consulta);
+    }
+}
